93e3913cf280e79e3d24dfe33f572b3029579628
SHASVATH SK <136348545+shasvathsk@users.noreply.github.com>
2026-02-11 13:39:20 +0530

    Time: 14 ms (85.71%), Space: 93.7 MB (39.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> map =new HashSet<>();
+        for(int i :nums){
+            if(map.contains(i)){
+            return true;
+            }
+            map.add(i);
+        }
+      return false;
+}}
