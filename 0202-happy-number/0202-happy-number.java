ca6bb7b860019c05ac7d492fff13b0441d666190
SHASVATH SK <136348545+shasvathsk@users.noreply.github.com>
2026-02-11 13:39:58 +0530

    Time: 0 ms (100%), Space: 42.5 MB (46.99%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n<10){
+            if(n==1 || n==7){
+                return true;
+            }
+            return false;
+        }
+        int a=n;
+        int sum=0;
+        while(n!=0){
+            sum=sum+(n%10)*(n%10);
+            n/=10;
+        }
+        return isHappy(sum);
+    }
+}
