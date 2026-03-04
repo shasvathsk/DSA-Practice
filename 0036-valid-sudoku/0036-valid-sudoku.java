c6a7d062da8c8da6b6daced5c9d542a81c0459e1
SHASVATH SK <136348545+shasvathsk@users.noreply.github.com>
2026-03-04 16:30:26 +0530

    Time: 3 ms (51.06%), Space: 46.6 MB (53.44%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row=new HashSet[9];
+        HashSet<Character>[] col=new HashSet[9];
+        HashSet<Character>[] grid=new HashSet[9];
+        for(int i=0;i<9;i++){
+            row[i]=new HashSet<>();
+            col[i]=new HashSet<>();
+            grid[i]=new HashSet<>();
+        }
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char ch=board[i][j];
+                if(ch=='.')continue;
+
+                if(row[i].contains(ch))return false;
+                row[i].add(ch);
+                if(col[j].contains(ch))return false;
+                col[j].add(ch);
+                int ind=(i/3)*3+(j/3);
+                if(grid[ind].contains(ch)){
+                    return false;
+                }
+                grid[ind].add(ch);
+            }
+        }
+        return true;
+    }
+}
