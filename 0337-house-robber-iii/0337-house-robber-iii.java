50d0395579ae5cf36d37bdb3c5e4a097a1d65b6c
SHASVATH SK <136348545+shasvathsk@users.noreply.github.com>
2026-03-12 12:03:50 +0530

    Time: 0 ms (100%), Space: 46.2 MB (94.66%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -1,24 +1,16 @@
 class Solution {
     public int rob(TreeNode root) {
-        int[] result = helper(root);
-        // The answer is the max of the two options at the root
+        int[] result = robb(root);
         return Math.max(result[0], result[1]);
     }
 
-    private int[] helper(TreeNode node) {
-        // Base case: If there's no house, we get $0 for both options
+    private int[] robb(TreeNode node) {
         if (node == null) return new int[]{0, 0};
+        int[] left = robb(node.left);
+        int[] right = robb(node.right);
 
-        // Recurse down to the children first (Post-order)
-        int[] left = helper(node.left);
-        int[] right = helper(node.right);
-
-        // Option 1: Rob this node
-        // Current value + children's "NotRobbed" values
         int robThis = node.val + left[1] + right[1];
 
-        // Option 2: Do NOT rob this node
-        // Take the max from the left child + max from the right child
         int notRobThis = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
 
         return new int[]{robThis, notRobThis};
