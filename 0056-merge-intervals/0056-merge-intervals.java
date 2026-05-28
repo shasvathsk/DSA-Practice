6cc86859af44fa10aa82400fb1ab5db919718997
SHASVATH SK <136348545+shasvathsk@users.noreply.github.com>
2026-05-28 22:26:47 +0530

    Time: 8 ms (90.64%), Space: 49 MB (70.61%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] current = intervals[0];
+        merged.add(current);
+        for (int[] interval : intervals) {
+            if (interval[0] <= current[1]) {
+                current[1] = Math.max(current[1], interval[1]);
+            } else {
+                current = interval;
+                merged.add(current);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
